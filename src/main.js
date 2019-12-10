a86268c732586efb9e249a22a9c1dbbef60f446c
guihua.ran <guihua.ran@ndmicro.com>
2019-12-10 22:23:37 +0800

    生产环境不展示调试工具

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Wedding from './Wedding.vue'
 import VConsole from 'vconsole'
 
-new VConsole()
+if( process.env.NODE_ENV === 'development') new VConsole()
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
